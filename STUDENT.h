fec93cfbfad81ce848f142a3a318a12de9cc47e5
mbobrov9 <43728323+mbobrov9@users.noreply.github.com>
2018-10-22 22:01:15 +0300

    first commit

---
+++ STUDENT.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+
+using namespace std;
+
+class STUDENT
+{
+private:
+	string Name;
+	string Group;
+	int progress[5];
+public:
+	STUDENT();
+	STUDENT(const STUDENT*);
+	~STUDENT();
+	void show();
+	void showdebtor();
+	char alfavit();
+}; 
