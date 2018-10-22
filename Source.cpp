06eab25564f09a0dea13002c76f0e9b1acc4e372
mbobrov9 <43728323+mbobrov9@users.noreply.github.com>
2018-10-22 23:38:34 +0300

    func

---
+++ Source.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "STUDENT.h"
+
+using namespace std;
+
+STUDENT::STUDENT()
+{
+	cout << "Class STUDENT creator" << endl;
+	cout << "Enter name of STUDENT: ";
+	cin >> Name;
+	cout << "Enter Group of the STUDENT: ";
+	cin >> Group;
+	cout << "Enter the progress:";
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> progress[i];
+	}
+}
+
+STUDENT::STUDENT(const STUDENT* P)
+{
+	cout << "copy ctor" << endl;
+
+	this->Name = P->Name;
+	this->Group = P->Group;
+	for (int i = 0; i < 5; i++)
+	{
+		this->progress[i] = P->progress[i];
+	}
+}
+
+STUDENT :: ~STUDENT()
+{
+	cout << "STUDENT destructor" << endl;
+}
+
+void STUDENT::show()
+{
+	cout << "Name of the STUDENT:" << Name << endl;
+	cout << "Name of the Group:" << Group << endl;
+	for (int i = 0; i < 5; i++)
+	{
+			cout << "progress:" << progress[i] << endl;
+	}
+}
+void STUDENT::showdebtor()
+{
+	int flag = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		if (progress[i] == 2)
+			flag = 1;
+	}
+	if (flag==1)
+	cout << "Name of the STUDENT:" << Name << endl;
+	cout << "Name of the Group:" << Group << endl;
+}
+char STUDENT::alfavit()
+{
+	char first = Name[0];
+	return first;
+}
